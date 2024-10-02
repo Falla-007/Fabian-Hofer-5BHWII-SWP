2845bd1d5946961575399eca402fd63252453857
Fabian Hofer <101357369+Falla-007@users.noreply.github.com>
2024-10-02 17:37:59 +0200

    Update Lottoaufgabe.py
    Ausgebessert

--- Lottoaufgabe.py
+++ Lottoaufgabe.py
@@ -13,17 +13,13 @@ def statistik_aktualisieren(statistik_dict, gezogene_zahlen):
         statistik_dict[zahl] += 1
 
 def lottoziehung():
-    # Initialisiere das Statistik-Dictionary mit allen Zahlen von 0 bis 45 auf 0
     statistik = {zahl: 0 for zahl in range(46)}
 
-    # Wiederhole die Ziehung 1000 Mal
     for i in range(1000):
         gezogene_zahlen = zufallszahlen()
         statistik_aktualisieren(statistik, gezogene_zahlen)
 
-    # Ausgabe der Statistik
     for zahl, anzahl in statistik.items():
         print(f"{zahl} ist {anzahl} mal gezogen worden")
 
-# Führe die Lottoziehung durch
 lottoziehung()
